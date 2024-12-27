ab67d6d263cc4b97afed6e3a7802fd55e0d50b3a
xacer <chacecaven@outlook.com>
2024-12-27 15:37:37 -0500

    add icon, resume, and links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,12 @@
             <p class="text-right right-0 text-xl">CS + Math</p>
             <p class="text-right right-0 text-xl">Georgia Institute of Technology</p>
 
+            <p class="text-right right-0 text-xl">
+                <a href="https://www.linkedin.com/in/chace-caven" target="_blank" class="underline text-blue-500" rel="noopener noreferrer">LinkedIn</a>  •
+                <a href="https://github.com/ccaven" target="_blank" class="underline text-blue-500" rel="noopener noreferrer">GitHub</a>  •
+                <a href="/Resume_Winter_2024.pdf" target="_blank" class="underline text-blue-500" rel="noopener noreferrer">CV</a>
+            </p>
+
         </div>
         <!-- Right panel -->
         <div class="pl-10">
@@ -31,8 +37,15 @@
                 </p>
 
                 <p class="text-xl py-2">
-                    Outside of work and research, I play club tennis, compete in hackathons, read books, and go on side quests to fix
-                    my own visual system.
+                    Outside of work and research, I play club tennis, compete in hackathons, read books, 
+                    and <a href="https://www.linkedin.com/posts/activity-7262164349522255872-KfsQ?utm_source=share&utm_medium=member_desktop" class="underline text-blue-500" target="_blank">go on side quests</a>
+                    to improve my own visual system.
+                </p>
+
+                <p class="text-xl py-2">
+                    My main strength is knowing what to do when there is nobody there to tell me what to do.
+                    The majority of my projects are self-directed, and I actively practice managing
+                    a full schedule and innovating under pressure.
                 </p>
 
                 <div class="grid grid-cols-4">
@@ -46,19 +59,19 @@
                         src="chace winning hackgt.jpg" 
                         class="w-full overflow-clip" 
                         alt="Chace wearing a Pupil Labs Neon eye tracker."
-                        title="Chace wearing a Pupil Labs eye tracker in the CODA building at Georgia Tech"
+                        title="Chace with two friends after winning HackGT"
                     >
                     <img 
                         src="chace in custom starship costume.jpg" 
                         class="w-full overflow-clip" 
                         alt="Chace wearing a Pupil Labs Neon eye tracker."
-                        title="Chace wearing a Pupil Labs eye tracker in the CODA building at Georgia Tech"
+                        title="Chace wearing his halloween costume"
                     >
                     <img 
                         src="chace with eye tracker.jpg" 
                         class="w-full overflow-clip" 
                         alt="Chace wearing a Pupil Labs Neon eye tracker."
-                        title="Chace wearing a Pupil Labs eye tracker in the CODA building at Georgia Tech"
+                        title="Chace wearing a Pupil Labs eye tracker"
                     >
                 </div>
                 
@@ -94,25 +107,7 @@
                     a generative model of vision and spatial information. Read the report <a href="https://api.wandb.ai/links/ccaven/cfhae2y9" class="underline text-blue-500" target="_blank">here</a>.
                 </p>
             </div>
-            <div>
-                <h2 class="text-2xl py-2 italic">Self-supervised pretraining of audio spectrogram transformers</h2>
-                <p class="text-xl py-2">
-                    Recorded speech can, in theory, be decomposed into three attributes: 
-                    the speech itself (words, emotion, pacing), 
-                    the speaker (i.e., the physical structures that produced the sounds), 
-                    and the background noise.
-                </p>
-                <p class="text-xl py-2">
-                    This project was all about designing machine learning architectures and objectives 
-                    to "pre-train" a model on 100,000 hours of unlabelled speech audio such that it
-                    generalizes better on tasks where only 100 hours of labeled audio exists.
-                </p>
-                <p class="text-xl py-2">
-                    In addition to machine learning methods, I wrote a custom data engine that streams audio
-                    from the internet, performs augmentations, and dispatches training commands 413x faster than real-time.
-                    The entire pipeline is written in safe, synchronous Rust (the best programming language).
-                </p>
-            </div>
+            
             <div>
                 <h2 class="text-2xl py-2 italic">Evolving small liquid neural networks to uncover neural population dynamics</h2>
                 <p class="text-xl py-2">
@@ -139,15 +134,51 @@
                 <p class="text-xl py-2">
                     How do you optimize the structure of a graph? That seems like a non-differentiable task. Instead of gradient descent,
                     I am <a href="https://www.linkedin.com/posts/vincere-bio_tbt-ai-science-activity-7102279759610433539-x7M2?utm_source=share&utm_medium=member_desktop" class="underline text-blue-500" target="_blank">going back to my roots</a>
-                    and using a fancy genetic algorithm for graph structure discovery called 
+                    and using a genetic algorithm for structure discovery called 
                     <a href="https://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf" class="underline text-blue-500" target="_blank" rel="noopener noreferrer">NEAT</a>.
-                    I hope to publish my first preprint in 2025, so stay tuned.
+                    I hope to release my first preprint in 2025, so stay tuned.
                 </p>
             </div>
             <div>
                 <h2 class="text-2xl py-2 italic">Neurons that fire together, wire together: exploring backpropagation alternatives</h2>
                 <p class="text-xl py-2">
-                    
+                    I am interested in creating the digital equivalent of a neuron; a single unit which can be duplicated, wired up, and activated
+                    by sensory information. Ideally, the unit has some "learning" behavior that can be accomplished without backpropagating through
+                    the entire network. The removal of a backpropagating step also removes the distinction between training and testing.
+                </p>
+                <p class="text-xl py-2">
+                    The main challenge is devising an objective function for each unit. The only information available is (1) the incoming activations,
+                    (2) the weights of the network, and (3) the output activations.
+                </p>
+                <p class="text-xl py-2">
+                    What if the objective is simply to pattern match the incoming activations? I.e., determine a "vocabulary" of patterns
+                    and optimize the vocabulary to cover the incoming data. In this project, I design the mathematical formulation of such
+                    an objective and apply it to a small one-layer convolution network.
+                </p>
+                <p class="text-xl py-2">
+                    Here's a link to the Google Colab notebook: <a href="https://colab.research.google.com/drive/1R-l643sPIyKmT_TY9BN1T8Vzs26HcFc3?usp=sharing" target="_blank" class="underline text-blue-500">fire_together_wire_together.ipynb</a>.
+                </p>
+            </div>
+            <div>
+                <h2 class="text-2xl py-2 italic">Self-supervised pretraining of audio spectrogram transformers</h2>
+                <p class="text-xl py-2">
+                    Recorded speech can, in theory, be decomposed into three attributes: 
+                    the speech itself (words, emotion, pacing), 
+                    the speaker (i.e., the physical structures that produced the sounds), 
+                    and the background noise.
+                </p>
+                <p class="text-xl py-2">
+                    This project was all about designing machine learning architectures and objectives 
+                    to "pre-train" a model on 100,000 hours of unlabelled speech audio such that it
+                    generalizes better on tasks where only 100 hours of labeled audio exists.
+                </p>
+                <p class="text-xl py-2">
+                    In addition to machine learning methods, I wrote a custom data engine that streams audio
+                    from the internet, performs augmentations, and dispatches training commands 413x faster than real-time.
+                    The entire pipeline is written in safe, synchronous Rust (the best programming language).
+                </p>
+                <p class="text-xl py-2">
+                    This project took place during a summer internship, so the source code is not available.
                 </p>
             </div>
         </div>
